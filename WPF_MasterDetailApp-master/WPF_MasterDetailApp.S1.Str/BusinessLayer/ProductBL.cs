7c12245981fb80c2eeefa8503caf3a0b8eb34120
CapitalistFerrari <45477189+CapitalistFerrari@users.noreply.github.com>
2019-02-14 17:06:40 -0500

    Add files via upload

---
+++ WPF_MasterDetailApp-master/WPF_MasterDetailApp.S1.Str/BusinessLayer/ProductBL.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WPF_MasterDetailApp;
+using WPF_MasterDetailApp.Models;
+
+namespace WPF_MasterDetailApp.BusinessLayer
+{
+    public class ProductBL
+    {
+        #region ENUMS
+
+
+
+        #endregion
+
+        #region FIELDS
+
+        ProductWindowView _productWindowView;
+        ProductWindowPresenter __productWindowPresenter;
+
+        #endregion
+
+        #region PROPERTIES
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        public ProductBL()
+        {
+            //
+            // instantiate the view model and initialize the data set
+            //
+
+            __productWindowPresenter = new ProductWindowPresenter(GetCompanyData(), GetProductData());
+
+            //
+            // instantiate, set the data context, and show the Main Window
+            //
+            _productWindowView = new ProductWindowView(__productWindowPresenter);
+            _productWindowView.DataContext = __productWindowPresenter;
+            _productWindowView.Show();
+        }
+
+        private Product GetProductData()
+        {
+            return new Product()
+            {
+                Id = 1,
+                Guitar = "Stratocaster", 
+                Manufacturer = "Fender",
+                ManufactureYear = 1954,
+                guitarType = Product.GuitarType.Electric,
+                AverageAnnualGross = 33333,
+                HireDate = DateTime.Parse("1/22/1958"),
+                Description = "",
+                ImageFileName = "fenderstartvertical.jpg"
+
+
+        };
+        }
+
+        private Company GetCompanyData()
+        {
+            Company company = new Company
+            {
+                Name = "RH industries",
+                Address = "Crystal Lake",
+                City = "Beulah"
+            };
+
+            return company;
+        }
+
+        #endregion
+
+        #region METHODS
+
+
+
+        #endregion
+
+        #region EVENTS
+
+
+        #endregion
+
+    }
+}
---
+++ WPF_MasterDetailApp-master/WPF_MasterDetailApp.S1.Str/Models/Product.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MasterDetailApp.Models
+{
+    public class Product
+    {
+        #region ENUMS
+
+        public enum GuitarType { Electric, Acoustic, ElectricAcoutic }
+
+        #endregion
+
+        #region FIELDS
+
+        private int _id;
+        private string _guitar;
+        private string _manufacturer;
+        private int _manufactureyear;
+        private GuitarType _guitartype;
+        private string _imageFileName;
+        private string _description;
+        private DateTime _hireDate;
+        private double _averageAnnualGross;
+
+        #endregion
+
+        #region PROPERTIES
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Guitar
+        {
+            get { return _guitar; }
+            set
+            {
+                _guitar = value;
+                
+            }
+        }
+
+        public string Manufacturer
+        {
+            get { return _manufacturer; }
+            set
+            {
+                _manufacturer = value;
+                
+            }
+        }
+
+        public int ManufactureYear
+        {
+            get { return _manufactureyear; }
+            set { _manufactureyear = value; }
+        }
+
+        public GuitarType guitarType
+        {
+            get { return _guitartype; }
+            set { _guitartype = value; }
+        }
+
+        public string ImageFileName
+        {
+            get { return _imageFileName; }
+            set { _imageFileName = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public DateTime HireDate
+        {
+            get { return _hireDate; }
+            set { _hireDate = value; }
+        }
+
+        public double AverageAnnualGross
+        {
+            get { return _averageAnnualGross; }
+            set { _averageAnnualGross = value; }
+        }
+
+        public string FullName
+        {
+            get { return _guitar + " " + _manufacturer; }
+        }
+
+        public string ImageFilePath
+        {
+            get { return @"../Images/" + _imageFileName; }
+        }
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        public Product()
+        {
+
+        }
+
+        #endregion
+
+        #region METHODS
+
+
+        #endregion
+
+        #region EVENTS
+
+
+
+        #endregion
+    }
+}
